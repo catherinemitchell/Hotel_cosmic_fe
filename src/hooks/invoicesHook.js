cebca50e7a743744a4ffd4caaf0cfdf61f8fc9c8
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:47:00 -0300

    Initialize dev

---
+++ src/hooks/invoicesHook.js
@@ -0,0 +1,62 @@
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+
+export function useInvoices() {
+  const [state, dispatch] = useReducer(invoicesReducer, {
+    invoices: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    // Fetching invoices
+    axios({
+      method: "GET",
+      url: "/api/invoices",
+    })
+      .then(({ data }) => {
+        console.log("data", data);
+        dispatch({
+          type: "SET_INVOICES",
+          payload: data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const addInvoice = (invoice) => {
+    // Adding a new invoice
+
+    axios({
+      method: "POST",
+      url: "/api/invoices",
+      data: invoice,
+    })
+      .then(({ data }) => {
+        dispatch({
+          type: "ADD_INVOICE",
+          payload: data,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return { ...state, addInvoice };
+}
+
+export function invoicesReducer(state, action) {
+  switch (action.type) {
+    case "SET_INVOICES":
+      return {
+        ...state,
+        invoices: action.payload,
+        loading: false,
+      };
+    case "ADD_INVOICE":
+      return {
+        ...state,
+        invoices: [...state.invoices, action.payload],
+      };
+    default:
+      return state;
+  }
+}
