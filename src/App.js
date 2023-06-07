be80aa9c00e1022288b19082c8384d789ec1881b
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:46:49 -0300

    Initialize feature/routes

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import './App.css';
+import Home from './components/Home';
+
+
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
