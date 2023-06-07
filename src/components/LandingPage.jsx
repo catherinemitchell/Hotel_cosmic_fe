1a5d3f3d631afef33ce3ec6058079dbb34654e21
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:46:53 -0300

    Initialize feature/reservations

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles/navbar-styles.css"
+
+export default function LandingPage() {
+
+  return (
+    
+      <p className="landing-body">
+        <div> Landing page !</div>
+      </p>
+    
+  )
+};
