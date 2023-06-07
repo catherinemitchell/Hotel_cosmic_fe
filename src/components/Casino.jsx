50802425c1d1791dcd5ad2a0004030b877517a11
Catherine Mitchell <nicholscat@yahoo.com>
2023-06-07 11:47:15 -0300

    Initialize integration/rooms

---
+++ src/components/Casino.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import "./styles/services-styles.css";
+
+import Casino1 from "./images/casino.png";
+import Casino2 from "./images/casino2.png";
+import Casino3 from "./images/casino3.jpg";
+import Casino4 from "./images/casino4.jpg";
+
+import Carousel from 'react-bootstrap/Carousel';
+
+export default function Casino() {
+
+  return (
+
+    <div className="page-container">
+      <div className="container-half">
+        <Carousel>
+          <Carousel.Item interval={2500}>
+            <img
+              className="d-block w-100"
+              src={Casino1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Blackjack Tables</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2500}>
+            <img
+              className="d-block w-100"
+              src={Casino2}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Blackjack Tables</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2500}>
+            <img
+              className="d-block w-100"
+              src={Casino3}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Poker Tables</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2500}>
+            <img
+              className="d-block w-100"
+              src={Casino4}
+              alt="Fourth slide"
+            />
+            <Carousel.Caption>
+              <h3>Roulette</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://cdn.pixabay.com/photo/2014/02/05/19/58/blue-259458_1280.jpg"
+              alt="Fifth slide"
+            />
+            <Carousel.Caption>
+              <h3>Astonishing sights</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+        </Carousel>
+      </div>
+      <div className="container-half-lower">
+        <p>
+          <h2>Casinos</h2>
+          Theme Lorem ipsum dolor sit amet. Ex delectus incidunt sed dolor numquam qui voluptas voluptatem est blanditiis repudiandae eum saepe accusamus et tempora nobis.
+          33 tempore accusantium ut rerum earum qui dolorum quibusdam sit magni voluptates qui ducimus deleniti vel cumque magnam ad quas rerum.
+          Qui inventore inventore sit reprehenderit veniam et galisum iure qui repellendus voluptatibus a adipisci consequatur qui incidunt delectus.
+          Eum omnis aliquam et omnis delectus eum reprehenderit voluptas et eaque nulla ut repellendus quisquam. Aut saepe consectetur non atque maiores et aliquid voluptate.
+          Ut porro recusandae At neque voluptates aut quia dolorem non consectetur excepturi et autem nulla.
+          Eos quisquam minima ea dolores vitae et necessitatibus corporis 33 earum omnis! Ut doloremque quos et dolorem accusantium non doloremque dolor sit sint ipsum.
+          At quos architecto rem expedita harum in minima quisquam qui dolores facilis. Et aliquam dicta id internos sint est sunt ipsam ad corporis accusamus ut facere iusto.</p>
+      </div>
+      <div className="button-container">
+        <Link to="/Reservations">
+          <button className="room-button">Book Room</button>
+        </Link>
+      </div>
+      
+    </div>
+  );
+};
